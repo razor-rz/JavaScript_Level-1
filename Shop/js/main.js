97d62d5e42f3f1911353c208cf2743d72160ac80
razor-rz <58488904+razor-rz@users.noreply.github.com>
2020-02-12 17:43:35 +0300

    add cart

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -5,16 +5,7 @@ let IMGS = ['https://electrozon.ru/upload/resize/a8/a84cdb0af9adfa544ddffa8131c6
 
 let products = []
 
-/* function createProductsDTO(arr) {
-      arr = []
-      let length = IDS.length
-
-      for (let i = 0; i < length; i++) {
-          arr.push(createProduct(i))
-      }
-  }*/
-
-function createProduct(index) {
+function createProduct (index) {
     return {
         product_name: PRODUCTS_NAMES[index],
         price: PRICES[index],
@@ -27,7 +18,7 @@ function createProduct(index) {
                 <div class="product-inf">
                     <span class="product-name">${this.product_name}</span>
                     <span class="product-price">${this.price}</span>
-                    <button class="buy-btn"
+                    <button class="buy-btn" name="buy-btn"
                         data-id="${this.id_product}"
                         data-name="${this.product_name}"
                         data-price="${this.price}">
@@ -40,46 +31,22 @@ function createProduct(index) {
     }
 }
 
-/*
-return `   <div class="cart-product" data-id="${this.id_product}">
-                <img src="${this.img}" alt="${thiclass="product-img" width="100" heigh="80">
-                <div class="product-inf">           
-                    <span class="cart-product-name">${t</span>
-                    <span class="cart-product-quantity">Quantity:</span>
-                    <span class="cart-each-quantity">each</span>
-                    <span class="cart-price">${this.price}</span>
-                    <button class="cart-btn-del">X</button>
-                </div>
-            </div>`
-            
-            */
-        
-let cart = {
-    items: [],
-    container: '.cart',
-    
-}
-
-
 let catalog = {
     items: [],
     container: '.main',
     cart: null,
 
     init() {
+        this.cart = cart
         this._fetchItems()
         this._render()
-        
-        document.querySelector (this.container).addEventListener('click', (evt) => {
-            if(evt.target.classList.contains ('buy-btn')) {
-                let product = evt.target.dataset
-                console.log (`
-                Куплен ${product.name}, по цене ${+product.price}$
-            `)    
-            }    
-            
+
+
+        document.querySelector(this.container).addEventListener('click', evt => {
+            if (evt.target.name === 'buy-btn') {
+                this.cart.addProduct(evt.target.dataset)
+            }
         })
-        
     },
     _fetchItems() {
         let length = IDS.length
@@ -88,7 +55,6 @@ let catalog = {
             this.items.push(createProduct(i))
         }
     },
-
     _render() {
         let container = document.querySelector(this.container)
         let domString = ''
@@ -100,9 +66,103 @@ let catalog = {
     }
 }
 
+
+let cart = {
+    items: [],
+    shown: false,
+    sum: 0,
+    qua: 0,
+    container: '.cart-block',
+    itemsContainer: '.cart-items',
+
+    init() {
+        document.querySelector('#toggle-cart').addEventListener('click', () => {
+            cart.shown = !cart.shown
+            cart.render()
+            document.querySelector('.cart-block').classList.toggle('invisible')
+        })
+        
+        document.querySelector(this.container).addEventListener('click', evt => {
+            if (evt.target.name === 'del-btn') {
+                this.removeProduct(evt.target.dataset.id)
+            }
+        })
+    },
+    render() {
+        let container = document.querySelector(this.itemsContainer)
+        let domString = ''
+
+        this.items.forEach(item => {
+            domString += item.createTemplate()
+        })
+        container.innerHTML = domString
+        
+        document.querySelector ('#tot-sum').innerHTML = this.sum
+        document.querySelector ('#tot-qua').innerHTML = this.qua
+        
+        
+        
+    },
+    addProduct(product) {
+        let find = this.items.find (item => item.id_product === product.id)
+        if (!find) {
+           this.items.push (createCartItem (product.id, product.name, product.price)) 
+        } else {
+            find.quantity++    
+        }
+        this.checkTotal ()     
+        this.render ()
+    },
+    removeProduct(id) {
+        let find = this.items.find (item => item.id_product === id)
+        if (find.quantity === 1) {
+            this.items.splice (this.items.indexOf(find), 1)  
+        } else {
+            find.quantity--    
+        }
+        this.checkTotal ()
+        this.render ()
+    },
+    checkTotal () {
+        let s = 0
+        let q = 0
+        
+        this.items.forEach (item => {
+            q += item.quantity
+            s += item.quantity * item.price
+        })
+        
+        this.sum = s
+        this.qua = q
+    }
+    
+}
+
+function createCartItem (id, name, price,) {
+    return {
+        id_product: id,
+        price: +price,
+        product_name: name,
+        quantity: 1,
+        img: IMGS[id],
+        createTemplate() {
+            return `   
+        <div class="cart-item" data-id="${this.id_product}">
+            <img src="${this.img}" alt="${this.product_name}" width="100" height="80">
+                <div class="product-desc">
+                    <p class="product-title">${this.product_name}</p>
+                    <p class="product-quantity">${this.quantity}</p>
+                    <p class="product-single-price">${this.price}</p>
+                </div>
+                <div class="right-block">
+                    <p class="product-total-price">$${this.price * this.quantity}</p>
+                    <button name="del-btn" class="del-btn" data-id="${this.id_product}">&times;</button>
+                </div> 
+        </div>
+        `
+        }
+    }
+}
+
+cart.init()
 catalog.init()
-
-
-
-
-
